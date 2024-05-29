94e24867cf1d6ecaec790428a24e1091d8b4e173
4lskittta <168069665+4lskittta@users.noreply.github.com>
2024-05-29 18:18:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
  // localhostにDenoのHTTPサーバーを展開
  import { serveDir } from "https://deno.land/std@0.223.0/http/file_server.ts";
  let previousWord = "しりとり";
+ let wordHistories = ["しりとり"];
+
 
  Deno.serve(async (request)=>{
      const pathname = new URL(request.url).pathname;
@@ -21,27 +23,63 @@
  
          // previousWordの末尾とnextWordの先頭が同一か確認
          if (previousWord.slice(-1) === nextWord.slice(0, 1)) {
+            if(nextWord.slice(-1)==="ん"){
+                return new Response(
+                    JSON.stringify({
+                        "errorMessage2":"ゲーム終了",
+                        "errorCode":"10002"
+                    }),
+                    {
+                        status:402,
+                        headers:{"Content-Type":"application/json; charset=utf-8"},
+                    }
+                );
+             } 
+            else if(wordHistories.includes(nextWord)){
+                return new Response(
+                    JSON.stringify({
+                        "errorMessage3":"既に入力された単語",
+                        "errorCode":"10003"
+                    }),
+                    {
+                        status:403,
+                        headers:{"Content-Type":"application/json; charset=utf-8"},
+                    }
+                );
+             } 
              // 同一であれば、previousWordを更新
              previousWord = nextWord;
+             wordHistories.push(previousWord);
          }
-         // 同一でない単語の入力時に、エラーを返す
+         // 同一でない単語の入力時に、エラーを返す             
+      
          else {
              return new Response(
                  JSON.stringify({
-                     "errorMessage": "前の単語に続いていません",
+                     "errorMessage1": "前の単語に続いていません",
                      "errorCode": "10001"
                  }),
                  {
-                     status: 400,
+                     status: 401,
                      headers: { "Content-Type": "application/json; charset=utf-8" },
                  }
              );
-         }
-　　　　　
+         } 
  
          // 現在の単語を返す
          return new Response(previousWord);
      }
+          // POST /reset: リセットする
+          // request.methodとpathnameを確認
+    if (request.method === "POST" && pathname === "/reset") {
+
+              // 既存の単語の履歴を初期化する
+              // 初期化した単語を返す
+        previousWord = "しりとり";
+        wordHistories = ["しりとり"]; 
+        return new Response("Game reset successfully.");     
+      }
+               
        // ./public以下のファイルを公開
      return serveDir(
          request,
